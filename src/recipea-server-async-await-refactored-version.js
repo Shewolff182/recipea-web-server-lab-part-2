7149aaf1ad3b42b399950d2a64109e7a6ea20d41
shan wolff <shanwolff182@gmail.com>
2023-12-12 12:58:19 -0500

    initial commit

--- src/recipea-server-async-await-refactored-version.js
+++ src/recipea-server-async-await-refactored-version.js
@@ -28,6 +28,23 @@ const deleteRecipe = async (id) => {
   await fs.writeFile("../data/recipea-data.json", jsonRecipes);
 };
 
+const updateRecipe = async (id, updatedRecipe) => {
+  const data = await fs.readFile("../data/recipea-data.json", "utf8");
+  const recipes = JSON.parse(data).map((recipe, i) => {
+    return i === id ? updatedRecipe : recipe;
+  });
+
+  const jsonVersion = JSON.stringify(recipes, null, 2);
+  await fs.writeFile("../data/recipea-data.json", jsonVersion, "utf8");
+};
+
+const createRecipe = async (newRecipe) => {
+  const data = await fs.readFile("../data/recipea-data.json", "utf8");
+  const recipes = [...JSON.parse(data), newRecipe];
+  const jsonVersion = JSON.stringify(recipes, null, 2);
+  await fs.writeFile("../data/data.json", jsonVersion, "utf8");
+};
+
 app.get("/find-recipes", async (req, res) => {
   const recipes = await getRecipes();
   res.send(recipes);
@@ -44,4 +61,19 @@ app.get("/trash-recipe/:id", async (req, res) => {
   const id = Number(req.params.id);
   await deleteRecipe(id);
   res.send("Recipe with " + id + " has been deleted.");
+});
+
+app.get("/update-recipe/:id", async (req, res) => {
+  const updatedRecipe = {
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  await updateRecipe(Number(req.params.id), updatedRecipe);
+  res.send(updatedRecipe);
+});
+
+app.get("/create-recipe", async (req, res) => {
+  await createRecipe({title: req.body.title, text: req.body.text});
+  res.send("Recipe successfully written to the file!");
 });
